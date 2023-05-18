585b88bb985eda3e4aa836d0735569eed533d20e
IgorOsipov <awesomeigor@gmail.com>
2023-05-19 02:33:45 +0300

    added navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+
+      <v-list :items="items">
+        <v-list nav>
+          <v-list-item v-for="item in items" :key="item.value" :prepend-icon="item.icon" :title="item.title"
+            :to="item.path">
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>TODO List</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data: () => (
+    {
+      drawer: false,
+      items: [
+        {
+          icon: "mdi-home-variant",
+          title: "Home",
+          path: "/"
+        },
+        {
+          icon: "mdi-information-variant",
+          title: "About",
+          path: "/about"
+        }
+      ]
+    }
+  ),
 }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
