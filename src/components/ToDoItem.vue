52f280f3fb83f8a6288e7b839ee8858422117826
IgorOsipov <awesomeigor@gmail.com>
2023-05-19 18:17:26 +0300

    added delete task button

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -17,15 +17,24 @@
       <v-timeline density="compact" align="start">
         <v-timeline-item
           v-for="message in messages"
-          :key="message.time"
+          :key="message.id"
           :dot-color="message.color"
-          size="x-small"
+          size="30"
+          width="100%"
         >
-          <div class="mb-4">
+          <div class="d-flex justify-space-between align-center">
             <div class="font-weight-normal">
               <strong>{{ message.from }}</strong> @{{ message.time }}
             </div>
-            <div>{{ message.message }}</div>
+            <v-btn
+              class="ma-2"
+              variant="text"
+              icon="mdi-close"
+              color="blue-lighten-2"
+              size="30"
+              style="margin: 0 !important"
+              @click="deleteTask(message.id)"
+            />
           </div>
         </v-timeline-item>
       </v-timeline>
@@ -34,28 +43,35 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+  import { ref } from "vue";
 
-const messages = ref([
-  {
-    from: "You",
-    message: `Sure, I'll see you later.`,
-    time: "10:42am",
-    color: "deep-purple-lighten-1",
-  },
-  {
-    from: "John Doe",
-    message: "Yeah, sure. Does 1:00pm work?",
-    time: "10:37am",
-    color: "green",
-  },
-  {
-    from: "You",
-    message: "Did you still want to grab lunch today?",
-    time: "9:47am",
-    color: "deep-purple-lighten-1",
-  },
-]);
+  const messages = ref([
+    {
+      id: 1,
+      from: "You",
+      message: `Sure, I'll see you later.`,
+      time: "10:42am",
+      color: "deep-purple-lighten-1",
+    },
+    {
+      id: 4,
+      from: "John Doe",
+      message: "Yeah, sure. Does 1:00pm work?",
+      time: "10:37am",
+      color: "green",
+    },
+    {
+      id: 3,
+      from: "You",
+      message: "Did you still want to grab lunch today?",
+      time: "9:47am",
+      color: "deep-purple-lighten-1",
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    messages.value = messages.value.filter((m) => m.id !== id);
+  };
 </script>
 
 <style></style>
